76d4e6c5624543f341c810eb139b6d6a78e3147d
kapiLveraM <117080546+kapiLveraM@users.noreply.github.com>
2023-12-25 12:45:37 +0530

    update code

--- src/components/common/Helper.jsx
+++ src/components/common/Helper.jsx
@@ -1,6 +1,34 @@
-export const navLinks= [
+import projects_img from "../assets/images/png/chert NOde.png"
+import projects_img2 from "../assets/images/png/Protect.png";
+import projects_img3 from "../assets/images/png/kahoot.png";
+
+export const navLinks = [
     { to: "/", label: "home" },
     { to: "/project", label: "works" },
     { to: "/about", label: "about-me" },
     { to: "/contact", label: "contacts" },
-  ];
+];
+  
+export const projects = [
+  {
+    img: projects_img,
+    skill: "HTML SCSS Python Flask",
+    projectsName: "ChertNodes",
+    servers: " Minecraft servers hosting",
+    btn: "Live",
+  },
+  {
+    img: projects_img2,
+    skill: "HTML SCSS Python Flask",
+    projectsName: "ProtectX",
+    servers: " Discord anti-crash bot",
+    btn: "Live",
+  },
+  {
+    img: projects_img3,
+    skill: "HTML SCSS Python Flask",
+    projectsName: "Kahoot Answers Viewer",
+    servers: "Get answers to your kahoot quiz",
+    btn: "Live",
+  },
+];
